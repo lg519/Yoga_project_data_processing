c6204efbf8086981944f1c401e3b3c90f1189412
lg519 <lg519@ic.ac.uk>
2023-09-09 15:59:30 +0100

    3 grids plotting

--- Process_EMG_data/muscle_activation_heat_map.py
+++ Process_EMG_data/muscle_activation_heat_map.py
@@ -35,6 +35,38 @@ def plot_heatmap(activations, active_channels, title, save_path):
     plt.close()
 
 
+def plot_combined_heatmap(activations_list, active_channels_list, title, save_path):
+    fig, axes = plt.subplots(1, len(activations_list), figsize=(15, 5))
+
+    for idx, (ax, activations, active_channels) in enumerate(
+        zip(axes, activations_list, active_channels_list)
+    ):
+        grid = np.zeros((8, 8))
+        for i, ch in enumerate(active_channels):
+            row = (ch - 1) // 8
+            col = (ch - 1) % 8
+            grid[row][col] = activations[i]
+        ax.imshow(grid, cmap="viridis", interpolation="nearest")
+        for i, ch in enumerate(active_channels):
+            row = (ch - 1) // 8
+            col = (ch - 1) % 8
+            ax.text(
+                col,
+                row,
+                str(ch),
+                ha="center",
+                va="center",
+                color="white" if activations[i] < 0.5 else "black",
+            )
+        ax.set_title(f"Grid {idx + 1}")  # Updated line
+        ax.axis("off")
+
+    plt.colorbar(ax.images[0], ax=axes, label="Muscle Activation", location="right")
+    plt.suptitle(title)
+    plt.savefig(save_path, dpi=300, bbox_inches="tight")
+    plt.close()
+
+
 def compute_exercise_activations(filenames, channel_indices, mvc_values):
     activations_per_exercise = defaultdict(lambda: [list() for _ in channel_indices])
 
@@ -159,24 +191,24 @@ if __name__ == "__main__":
     os.makedirs(save_directory, exist_ok=True)
 
     for exercise_name, all_activations in activations_per_exercise.items():
-        for grid_num, active_channels in grids:
-            for rep_index, _ in enumerate(all_activations[0]):
-                activations_start_idx = sum(
-                    [len(grid[1]) for grid in grids[: grid_num - 1]]
-                )
-                activations_end_idx = activations_start_idx + len(active_channels)
-                rep_activations = [
+        for rep_index in range(len(all_activations[0])):
+            combined_activations = [
+                [
                     np.mean(all_activations[i][rep_index])
-                    for i in range(activations_start_idx, activations_end_idx)
+                    for i in range(
+                        sum(len(grid[1]) for grid in grids[:index]),
+                        sum(len(grid[1]) for grid in grids[: index + 1]),
+                    )
                 ]
-
-                heatmap_save_path = os.path.join(
-                    save_directory,
-                    f"{participant_type} - Grid {grid_num} - {exercise_name} - Rep {rep_index + 1} - Heatmap.png",
-                )
-                plot_heatmap(
-                    rep_activations,
-                    active_channels,
-                    f"{participant_type} - Grid {grid_num} - {exercise_name} - Rep {rep_index + 1}",
-                    heatmap_save_path,
-                )
+                for index, (grid_num, active_channels) in enumerate(grids)
+            ]
+            heatmap_save_path = os.path.join(
+                save_directory,
+                f"{participant_type} - Combined Grids - {exercise_name} - Rep {rep_index + 1} - Heatmap.png",
+            )
+            plot_combined_heatmap(
+                combined_activations,
+                [grid[1] for grid in grids],
+                f"{participant_type} - Combined Grids - {exercise_name} - Rep {rep_index + 1}",
+                heatmap_save_path,
+            )
