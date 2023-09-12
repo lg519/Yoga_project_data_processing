391198e53e2b2079f962e3d1c60a42b9f069a390
lg519 <lg519@ic.ac.uk>
2023-09-12 11:10:12 +0100

    interactive html plots

--- Process_EMG_data/muscle_activations_per_exercise_different_reps_circles.py
+++ Process_EMG_data/muscle_activations_per_exercise_different_reps_circles.py
@@ -14,6 +14,8 @@ from apply_processing_pipeline import normalize_signal
 from amplifier_config import sampling_frequency, get_channel_names
 from utilis import get_mat_filenames, get_partecipant_type, get_exercise_name
 
+import plotly.graph_objects as go
+
 
 def plot_muscle_activation_per_exercise_different_reps(
     activations,
@@ -22,9 +24,6 @@ def plot_muscle_activation_per_exercise_different_reps(
     participant_type,
     save_directory,
 ):
-    plt.figure(figsize=(8, 8))
-    ax = plt.subplot(111, projection="polar")
-
     # Checks for empty reps and activations
     for reps, channel in zip(activations, channel_names):
         if len(reps) == 0:
@@ -35,38 +34,33 @@ def plot_muscle_activation_per_exercise_different_reps(
                     f"Empty rep #{rep_num} in Exercise: {exercise_name}, Channel: {channel}"
                 )
 
-    # Get the number of reps for the first channel (assuming consistent repetitions across channels)
+    # Get the number of reps for the first channel
     num_reps = len(activations[0])
-    colors = plt.cm.tab10(np.linspace(0, 1, num_reps))
 
     # Calculate mean activations for the entire exercise
     mean_activations = [np.mean([np.mean(rep) for rep in reps]) for reps in activations]
 
-    # Convert channel positions to radians for polar plotting
-    theta = np.linspace(0.0, 2 * np.pi, len(channel_names), endpoint=False)
+    # Convert channel positions to degrees for polar plotting
+    theta = np.linspace(0.0, 360, len(channel_names), endpoint=False)
+    theta = np.append(theta, 0)  # Close the circle by adding the starting point again
+
+    fig = go.Figure()
 
     # Plot average activation with dotted lines
-    ax.plot(
-        np.concatenate(
-            [theta, [theta[0]]]
-        ),  # Repeat the first theta to close the circle
-        mean_activations
-        + [mean_activations[0]],  # Repeat the first value to close the circle
-        linestyle="--",
-        color="black",  # Black color for clarity
-        label="Mean Activation",
-        alpha=0.7,
+    fig.add_trace(
+        go.Scatterpolar(
+            r=mean_activations + [mean_activations[0]],
+            theta=theta,
+            mode="lines",
+            name="Mean Activation",
+            line=dict(color="black", dash="dash"),
+        )
     )
 
+    # Colors for repetitions. If more repetitions are present, consider expanding this list or generating it programmatically.
+    colors = plt.cm.tab10(np.linspace(0, 1, num_reps))
     # Plot lines connecting the mean activation for each repetition across channels
     for rep_index in range(num_reps):
-        # Check for empty activation
-        for channel in range(len(channel_names)):
-            if len(activations[channel]) <= rep_index:
-                print(
-                    f"Empty activation for Exercise: {exercise_name}, Channel: {channel_names[channel]}, Rep: {rep_index}"
-                )
-
         activations_for_rep = [
             np.mean(activations[channel][rep_index])
             if len(activations[channel]) > rep_index
@@ -74,38 +68,32 @@ def plot_muscle_activation_per_exercise_different_reps(
             for channel in range(len(channel_names))
         ]
 
-        ax.plot(
-            np.concatenate(
-                [theta, [theta[0]]]
-            ),  # Repeat the first theta to close the circle
-            activations_for_rep
-            + [activations_for_rep[0]],  # Repeat the first value to close the circle
-            marker="o",
-            color=colors[rep_index],
-            label=f"Rep {rep_index + 1}",
+        fig.add_trace(
+            go.Scatterpolar(
+                r=activations_for_rep + [activations_for_rep[0]],
+                theta=theta,
+                mode="lines+markers",
+                name=f"Rep {rep_index + 1}",
+                line=dict(color="rgba({},{},{},{})".format(*colors[rep_index], 1)),
+            )
         )
 
-    font_properties = {"fontsize": 12, "fontweight": "bold"}
-
-    # Split channel names at the first opening bracket for better display
-    channel_names = [
-        name.replace(" (", "\n(") if "(" in name else name for name in channel_names
-    ]
-
-    ax.set_xticks(theta)
-    ax.set_xticklabels(channel_names, **font_properties)
-    ax.set_title(
-        f"{participant_type} - {exercise_name}", va="bottom", **font_properties
+    fig.update_layout(
+        title=f"{participant_type} - {exercise_name}",
+        polar=dict(
+            radialaxis=dict(visible=True),
+            angularaxis=dict(
+                tickvals=np.linspace(0, 360, len(channel_names), endpoint=False),
+                ticktext=channel_names,
+            ),
+        ),
     )
-    ax.legend(loc="upper right")
 
-    # Save the plot
-    plt.tight_layout()
+    # Save the plot as an interactive HTML
     plot_filename = os.path.join(
-        save_directory, f"{participant_type} - {exercise_name}.png"
+        save_directory, f"{participant_type} - {exercise_name}.html"
     )
-    plt.savefig(plot_filename)
-    plt.close()
+    fig.write_html(plot_filename)
 
 
 def compute_exercise_activations(filenames, channel_indices, mvc_values):
